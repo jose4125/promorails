0405c01d6638b9a764cb88e80d6c2c1e30e33ba9
jose david lombana <jose4125@gmail.com>
2013-05-02 19:02:25 -0500

    promo files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Promorails::Application.config.secret_token = '6cd274bedc5f6c1ebf4661c7f3a39a18396d28ea706f299c4124d2aff0b811843765b24d354ce526364bd403130c8c8b33b45026fe39ebbd56cce8bf715c53e6'
