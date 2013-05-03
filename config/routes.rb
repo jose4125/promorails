f51c844d53f712af685973c4b67906f2eb5ea885
jose david lombana <jose4125@gmail.com>
2013-05-03 17:27:34 -0500

    controlador y vista de sessiones - login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Promorails::Application.routes.draw do
 
   resources :users
   resources :frases, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
 
   # The priority is based upon order of creation:
@@ -54,6 +55,7 @@ Promorails::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+   root :to => 'sessions#new'
 
   # See how all your routes lay out with "rake routes"
 
