78e685967fc3497f9fdc35707ea114f92ea365ad
jose david lombana <jose4125@gmail.com>
2013-05-05 00:29:51 -0500

    ingresar una sola frase con helpers en la vista

--- app/helpers/frases_helper.rb
+++ app/helpers/frases_helper.rb
@@ -1,2 +1,10 @@
 module FrasesHelper
+  
+  def user_frase?
+    @frase = Frase.find_by_user_id( current_user.id)
+  end
+
+  def estado_frase
+    ( @frase.valida ) ? 'correcta' : 'incorrecta'
+  end
 end
